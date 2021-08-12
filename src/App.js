8a83c4b9b159b415e10f593d6cc12a68abf8f9ff
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-08-12 15:09:26 -0400

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header'; 
+// import About from './components/About'; 
 
 const App = () => {
   return (
     <div>
       <Header />
+      {/* <About /> */}
       
     </div>
     
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,56 +1,32 @@
-import React, { useState } from 'react'; 
 import {
-    Collapse,
     Navbar,
-    NavbarToggler,
     NavbarBrand,
     Nav,
     NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    NavbarText
+    NavLink
 } from 'reactstrap'; 
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen); 
+
     return (
         <div>
           <Navbar color="light" light expand="md">
-            <NavbarBrand href="/">ANR</NavbarBrand>
-            <NavbarToggler onClick={toggle} />
-            <Collapse isOpen={isOpen} navbar>
+            <NavbarBrand href="/" style={{marginLeft:"20px", fontWeight:"bolder"}}>ANR</NavbarBrand>
               <Nav className="mr-auto" navbar>
                 <NavItem>
-                  <NavLink href="/components/">Components</NavLink>
+                  <NavLink href="/components/">About</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                  <NavLink href="/components/">Projects</NavLink>
                 </NavItem>
-                <UncontrolledDropdown nav inNavbar>
-                  <DropdownToggle nav caret>
-                    Options
-                  </DropdownToggle>
-                  <DropdownMenu right>
-                    <DropdownItem>
-                      Option 1
-                    </DropdownItem>
-                    <DropdownItem>
-                      Option 2
-                    </DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem>
-                      Reset
-                    </DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
+                <NavItem>
+                  <NavLink href="/components/">Contact</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/components/">Resume</NavLink>
+                </NavItem>
               </Nav>
-              <NavbarText>Simple Text</NavbarText>
-            </Collapse>
-          </Navbar>
+            </Navbar>
         </div>
     );
 }
