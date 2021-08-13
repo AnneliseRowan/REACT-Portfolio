ce54611c0e3c5fa4622ae2505eec93e1870c2a19
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-08-12 21:26:28 -0400

    got background image and name to appear

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,11 @@ const About = () => {
     return (
         <div className="container">
             <div className="bg-image" 
-            style={{"backgroundImage": "url('https://mdbootstrap.com/img/Photos/Others/images/76.webp')", "height": "100vh"}}
-            ></div>
+            style={{"backgroundImage": "url('https://mdbootstrap.com/img/Photos/Others/images/76.webp')", "height": "100vh"}}>
+                <div className="maintxt position-absolute top-50 start-50 translate-middle">
+                    Annelise Rowan
+                </div>
+            </div>
         </div>
     )
 
