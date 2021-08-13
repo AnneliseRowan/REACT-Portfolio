83f5a35359433bca364423bf851c9f72599df8c0
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-08-13 15:36:19 -0400

    added pics and links to project page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,61 +1,97 @@
 import React from 'react';
+import TailoristImage from "../../assets/tailorist.jpg";
+import FollowImage from "../../assets/followthereader.png";
+import TeamImage from "../../assets/teamprofilegenerator.png"; 
+import EmployeeTracker from "../../assets/employeetracker.png"; 
+import BudgetTracker from "../../assets/pwa.png"; 
+import WorkoutTracker from "../../assets/workout.png"; 
+import EcommerceBackEnd from "../../assets/ecommerce.png";
+import WeatherDashboard from "../../assets/weather.png"; 
 
 const Projects = () => {
     return (
     <div>
-        {/* Header */}
         <header className="bg-primary text-center py-5 mb-4">
         <div className="container">
-            <h1 className="fw-light text-white">Meet the Team</h1>
+            <h1 className="fw-light text-white">My Latest and Greatest Projects</h1>
         </div>
         </header>
-        {/* Page Content */}
         <div className="container">
         <div className="row">
-            {/* Team Member 1 */}
             <div className="col-xl-3 col-md-6 mb-4">
             <div className="card border-0 shadow">
-                <img src="https://source.unsplash.com/TMgQMXoglsM/500x350" className="card-img-top" alt="..." />
+                <img src={TailoristImage} className="card-img-top" alt="..." />
                 <div className="card-body text-center">
-                <h5 className="card-title mb-0">Team Member</h5>
-                <div className="card-text text-black-50">Web Developer</div>
+                <h5 className="card-title mb-0">The-Tailorist</h5>
+                <a className="social-icon" target="_blank." href="https://github.com/AnneliseRowan/the-tailorist"><i class="fab fa-github"></i></a>
                 </div>
             </div>
             </div>
-            {/* Team Member 2 */}
             <div className="col-xl-3 col-md-6 mb-4">
             <div className="card border-0 shadow">
-                <img src="https://source.unsplash.com/9UVmlIb0wJU/500x350" className="card-img-top" alt="..." />
+                <img src={FollowImage} className="card-img-top" alt="..." />
                 <div className="card-body text-center">
-                <h5 className="card-title mb-0">Team Member</h5>
-                <div className="card-text text-black-50">Web Developer</div>
+                <h5 className="card-title mb-0">Follow-The-Reader</h5>
+                <a className="social-icon" target="_blank." href="https://github.com/AnneliseRowan/project-2"><i class="fab fa-github"></i></a>
                 </div>
             </div>
             </div>
-            {/* Team Member 3 */}
             <div className="col-xl-3 col-md-6 mb-4">
             <div className="card border-0 shadow">
-                <img src="https://source.unsplash.com/sNut2MqSmds/500x350" className="card-img-top" alt="..." />
+                <img src={TeamImage} className="card-img-top" alt="..." />
                 <div className="card-body text-center">
-                <h5 className="card-title mb-0">Team Member</h5>
-                <div className="card-text text-black-50">Web Developer</div>
+                <h5 className="card-title mb-0">Team-Profile-Generator</h5>
+                <a className="social-icon" target="_blank." href="https://github.com/AnneliseRowan/team-profile-generator"><i class="fab fa-github"></i></a>
                 </div>
             </div>
             </div>
-            {/* Team Member 4 */}
             <div className="col-xl-3 col-md-6 mb-4">
             <div className="card border-0 shadow">
-                <img src="https://source.unsplash.com/7u5mwbu7qLg/500x350" className="card-img-top" alt="..." />
+                <img src={EmployeeTracker} className="card-img-top" alt="..." />
                 <div className="card-body text-center">
-                <h5 className="card-title mb-0">Team Member</h5>
-                <div className="card-text text-black-50">Web Developer</div>
+                <h5 className="card-title mb-0">Employee-Tracker</h5>
+                <a className="social-icon" target="_blank." href="https://github.com/AnneliseRowan/employee-tracker"><i class="fab fa-github"></i></a>
+                </div>
+            </div>
+            </div>
+            <div className="col-xl-3 col-md-6 mb-4">
+            <div className="card border-0 shadow">
+                <img src={BudgetTracker} className="card-img-top" alt="..." />
+                <div className="card-body text-center">
+                <h5 className="card-title mb-0">PWA-Online-Offline-Budget-Tracker</h5>
+                <a className="social-icon" target="_blank." href="https://github.com/AnneliseRowan/PWA-Online-Offline-Budget-Tracker"><i class="fab fa-github"></i></a>
+                </div>
+            </div>
+            </div>
+            <div className="col-xl-3 col-md-6 mb-4">
+            <div className="card border-0 shadow">
+                <img src={WorkoutTracker} className="card-img-top" alt="..." />
+                <div className="card-body text-center">
+                <h5 className="card-title mb-0">Personalized-Workout-Tracker</h5>
+                <a className="social-icon" target="_blank." href="https://github.com/AnneliseRowan/personalized-workout-tracker"><i class="fab fa-github"></i></a>
+                </div>
+            </div>
+            </div>
+            <div className="col-xl-3 col-md-6 mb-4">
+            <div className="card border-0 shadow">
+                <img src={EcommerceBackEnd} className="card-img-top" alt="..." />
+                <div className="card-body text-center">
+                <h5 className="card-title mb-0">E-Commerce-Back-End</h5>
+                <a className="social-icon" target="_blank." href="https://github.com/AnneliseRowan/e-commerce-back-end"><i class="fab fa-github"></i></a>
+                </div>
+            </div>
+            </div>
+            <div className="col-xl-3 col-md-6 mb-4">
+            <div className="card border-0 shadow">
+                <img src={WeatherDashboard} className="card-img-top" alt="..." />
+                <div className="card-body text-center">
+                <h5 className="card-title mb-0">Weather-Dashboard</h5>
+                <a className="social-icon" target="_blank." href="https://github.com/AnneliseRowan/weather-dashboard"><i class="fab fa-github"></i></a>
                 </div>
             </div>
             </div>
         </div>
-        {/* /.row */}
         </div>
-        {/* /.container */}
     </div>
     );
 }
