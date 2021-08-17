49142a30c167e7136f61447e4fc97c0f5522c4c0
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-08-17 18:23:04 -0400

    got the form to submit

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './contact.css';
-// import { validateEmail } from './utils/Helpers';
+import { validateEmail } from './utils/Helpers';
 
 // const Contact = () => {
 //   const [name, setName] = useState('');
@@ -85,64 +85,92 @@ import './contact.css';
 
 // export default Contact;
 
-class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      message: ''
+const Contact = () => { 
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     name: '',
+  //     email: '',
+  //     message: ''
+  //   }
+    
+  // }
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("hey were doing this handleForm")
+    if (!validateEmail(email) || !name) {
+      setErrorMessage('Email is invalid');
+      return;
     }
     
+    alert(`Hello ${name}`);
+
+    setName('');
+    setMessage('');
+    setEmail('');
+  };
+
+  const onNameChange = (event) => {
+    setName(event.target.value)
   }
 
-  
+  const onEmailChange = (event) => {
+    setEmail(event.target.value)
+  }
 
-  render() {
-    return(
-      <div>
-        <header className="bg- text-center py-5 mb-4">
-          <div className="container">
-              <h1 className="fw-light text-black" style={{fontFamily: "Lobster, cursive", fontSize: "64px"}} >Let's Chat!</h1>
-          </div>
-        </header>
-        <div className="App container">
-          <div className="row justify-content-center">
-            <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST" data-netlify="true">
-              <div className="form-group">
-                <label htmlFor="name" style={{fontFamily: "Lobster, cursive", fontSize:"24px"}}>Name</label>
-                <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
-              </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputEmail1" style={{fontFamily: "Lobster, cursive", fontSize:"24px"}}>Email address</label>
-                <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-              </div>
-              <div className="form-group">
-                <label htmlFor="message" style={{fontFamily: "Lobster, cursive", fontSize:"24px"}}>Message</label>
-                <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
-              </div>
-              <button type="submit" className="btn btn-secondary" style={{marginTop:"10px"}}>Submit</button>
-            </form>
-          </div>
+  const onMessageChange = (event) => {
+    setMessage(event.target.value)
+  }
+
+  return(
+    <div>
+      <header className="bg- text-center py-5 mb-4">
+        <div className="container">
+            <h1 className="fw-light text-black" style={{fontFamily: "Lobster, cursive", fontSize: "64px"}} >Let's Chat!</h1>
+        </div>
+      </header>
+      <div className="App container">
+        <div className="row justify-content-center">
+          <form id="contact-form" onSubmit={handleFormSubmit} method="POST" data-netlify="true">
+            <div className="form-group">
+              <label htmlFor="name" style={{fontFamily: "Lobster, cursive", fontSize:"24px"}}>Name</label>
+              <input type="text" className="form-control" value={name} onChange={onNameChange.bind()} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1" style={{fontFamily: "Lobster, cursive", fontSize:"24px"}}>Email address</label>
+              <input type="email" className="form-control" aria-describedby="emailHelp" value={email} onChange={onEmailChange.bind()} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message" style={{fontFamily: "Lobster, cursive", fontSize:"24px"}}>Message</label>
+              <textarea className="form-control" rows="5" value={message} onChange={onMessageChange.bind()} />
+            </div>
+            <button type="submit" className="btn btn-secondary" style={{marginTop:"10px"}}>Submit</button>
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 
-  onNameChange(event) {
-    this.setState({name: event.target.value})
-  }
-
-  onEmailChange(event) {
-    this.setState({email: event.target.value})
-  }
-
-  onMessageChange(event) {
-    this.setState({message: event.target.value})
-  }
-
-  handleSubmit(event) {
-  }
 }
 
-export default Contact;
+export default Contact
