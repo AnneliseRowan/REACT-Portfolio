92078992c044ae361acae73771f6f7d705a086d9
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-09-24 15:04:42 -0400

    updated resume

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="mt-3">
             <footer className="text-center py-5">
                 <div className="mt-5">
-                    <a href="https://docs.google.com/document/d/1zPXTHRwFn3A_XsrZz-JfS2GyD5WN0nOCpBq_QVi8y3A/edit?usp=sharing" target="blank">
+                    <a href="https://docs.google.com/document/d/1zNCA-aYvJaGYsiGWY0a8mV2IN48S19kogd5t9sLNMyw/edit?usp=sharing" target="blank">
                     <div className="fw-light text-black" style={{fontFamily: "Lobster, cursive", fontSize: "24px"}} >Download My Resume</div>
                     </a>
                     <span className="mt-5" style={{fontFamily: "Karla", fontSize: "18px"}}>Cell Number: (904) 325-0895  ||  Email: Annelise.Rowan@gmail.com</span>
